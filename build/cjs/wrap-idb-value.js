a054cae73fed953930524d78fa2e3fffafb9c104
Jeremy Scheff <jdscheff@gmail.com>
2020-10-17 14:40:36 -0400

    Force build, even if tests don't pass

--- build/cjs/wrap-idb-value.js
+++ build/cjs/wrap-idb-value.js
@@ -167,7 +167,7 @@ function transformCachableValue(value) {
 function wrap(value) {
     // We sometimes generate multiple promises from a single IDBRequest (eg when cursoring), because
     // IDB is weird and a single IDBRequest can yield many responses, so these can't be cached.
-    if (value instanceof IDBRequest)
+    if (value instanceof IDBRequest || (value && value.toString() === "[object IDBRequest]"))
         return promisifyRequest(value);
     // If we've already transformed this value before, reuse the transformed value.
     // This is faster, but it also provides object equality.
